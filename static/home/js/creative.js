b0099c8eac64de5bf8559de95fd77fbc23d997e1
hamidreza <hamidreza004@gmail.com>
2018-11-01 21:56:35 +0330

    fix prizes

--- static/home/js/creative.js
+++ static/home/js/creative.js
@@ -1,6 +1,7 @@
 (function ($) {
     "use strict"; // Start of use strict
 
+
     // Smooth scrolling using jQuery easing
     $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
         if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
@@ -195,7 +196,6 @@
 
 
     });
-
 
 })(jQuery); // End of use strict
 
