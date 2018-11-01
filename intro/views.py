7922469ff790efb60d0de3924cf4cf022d3b1a46
Arya Kowsary <aryakowsary@gmail.com>
2018-11-01 17:57:35 +0330

    check requests

--- intro/views.py
+++ intro/views.py
@@ -1,4 +1,4 @@
-from urllib.request import urlopen, Request
+import requests
 from bs4 import BeautifulSoup
 
 # Create your views here.
@@ -12,7 +12,6 @@ class HomeView(TemplateView):
 
     def get_context_data(self, *args, **kwargs):
         registration_url = "http://ssc.ce.sharif.edu/pages/payment-forms/%D8%AB%D8%A8%D8%AA-%D9%86%D8%A7%D9%85-%D8%AF%D8%B1-%D8%B1%D9%88%DB%8C%D8%AF%D8%A7%D8%AF-%D8%A7%DB%8C%D9%86%D8%AA%D8%B1%D9%86%D8%A7/"
-        client = Request(registration_url, headers={"User-Agent": "Mozilla/5.0"})
-        content = urlopen(client).read()
+        content = requests.get(registration_url).text
         soup = BeautifulSoup(content,features="html.parser")
         return {"numberOfTeams":toPersianDigit(soup.find('div', id='valid_count').text)}
