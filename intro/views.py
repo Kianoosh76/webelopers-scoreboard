4a215f6d8f520f22e10006673975b15a8bcd0ee3
Arya Kowsary <aryakowsary@yahoo.com>
2018-11-06 13:05:00 +0330

    Update views.py

--- intro/views.py
+++ intro/views.py
@@ -9,7 +9,7 @@ from intro.utils import toPersianDigit
 class HomeView(TemplateView):
     template_name = 'home.html'
 
-    def get_context_data(self, *args, **kwargs):
-        registration_url = "http://ce.sharif.edu/~golezardi/ssc-count/webelopers.php"
-        content = requests.get(registration_url).text
-        return {"numberOfTeams":toPersianDigit(content)}
+    #def get_context_data(self, *args, **kwargs):
+        #registration_url = "http://ce.sharif.edu/~golezardi/ssc-count/webelopers.php"
+        #content = requests.get(registration_url).text
+        #return {"numberOfTeams":toPersianDigit(content)}
