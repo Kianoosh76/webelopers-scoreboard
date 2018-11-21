b12ab3a995ec14545c01ded4db5dd14fe2f8d9a0
somagh <aryakowsary@yahoo.com>
2018-11-22 01:09:24 +0330

    check failed

--- webelopers_scoreboard/helpers.py
+++ webelopers_scoreboard/helpers.py
@@ -2,12 +2,7 @@
 def get_client_ip(request):
     x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
     if x_forwarded_for:
-        ips = x_forwarded_for.split(',')
-        print(ips)
-        for ip in ips:
-            if ip[:3] == '192':
-                return ip
-                # ip = x_forwarded_for.split(',')[0]
+        ip = x_forwarded_for.split(',')[0]
     else:
         ip = request.META.get('REMOTE_ADDR')
     return ip
