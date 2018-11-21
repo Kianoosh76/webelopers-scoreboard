710064fa9c69e4e8bdbb0f532bac615e7a93cab8
somagh <aryakowsary@yahoo.com>
2018-11-22 00:49:54 +0330

    check

--- webelopers_scoreboard/helpers.py
+++ webelopers_scoreboard/helpers.py
@@ -2,7 +2,12 @@
 def get_client_ip(request):
     x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
     if x_forwarded_for:
-        ip = x_forwarded_for.split(',')[0]
+        ips = x_forwarded_for.split(',')
+        print(ips)
+        for ip in ips:
+            if ip[:3] == '192':
+                return ip
+                # ip = x_forwarded_for.split(',')[0]
     else:
         ip = request.META.get('REMOTE_ADDR')
     return ip
--- webelopers_scoreboard/settings.py
+++ webelopers_scoreboard/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'dbbackup',
     'django_extensions',
     'intro',
     'teams',
